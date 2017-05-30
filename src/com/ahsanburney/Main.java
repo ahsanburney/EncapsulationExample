b703975f7973f44753764ce01da14cb244cad01e
ASP17SCM03B <aburney1@hawk.iit.edu>
2017-05-30 13:30:34 -0500

    Initial commit

---
+++ src/com/ahsanburney/Main.java
@@ -0,0 +1,11 @@
+package com.ahsanburney;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        EnhancedPlayer player = new EnhancedPlayer("BOB",200,"Sword");
+        System.out.println("Initial health is "+ player.getHealth());
+
+    }
+}
